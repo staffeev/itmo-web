b39a1069fe5f99536892fecaee85f70545d24bbd
staffeev <st.affeev@yandex.ru>
2025-10-13 02:05:02 +0300

    primitive html

--- semester1/lab2/logic.js
+++ semester1/lab2/logic.js
@@ -1,1 +1,7 @@
-console.log('Hello world!');
+let addBtn = document.querySelector("#add_task_btn");
+let nameEntry = document.querySelector("#add_task_name");
+let taskContainer = document.querySelector(".task-container");
+
+let tasks = [];
+
+
