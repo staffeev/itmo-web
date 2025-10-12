2379416540ae26ddacd0eaed621b2e3d86aa3b9e
staffeev <st.affeev@yandex.ru>
2025-10-13 02:20:25 +0300

    simple add and delete

--- semester1/lab2/logic.js
+++ semester1/lab2/logic.js
@@ -1,7 +1,35 @@
-let addBtn = document.querySelector("#add_task_btn");
-let nameEntry = document.querySelector("#add_task_name");
+const addBtn = document.querySelector("#add_task_btn");
+const nameEntry = document.querySelector("#add_task_name");
 let taskContainer = document.querySelector(".task-container");
 
 let tasks = [];
+
+
+function addTask() {
+    if(nameEntry.value === '') {
+        alert("чтобы добавить задачу, надо написать ее");
+    }else{
+        let task = document.createElement("li")
+        task.innerHTML = nameEntry.value;
+        taskContainer.appendChild(task);
+        let closeBtn = document.createElement("span");
+        closeBtn.innerHTML = "\u00d7";
+        task.appendChild(closeBtn);
+    }
+    nameEntry.value = '';
+}
+
+function deleteTask() {
+
+}
+
+
+addBtn.onclick = addTask;
+
+taskContainer.addEventListener("click", e => {
+    if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+})
 
 
